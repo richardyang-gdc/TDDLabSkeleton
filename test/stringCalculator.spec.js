fc7abf7f7c93550e5cf55b01222146cb0dc856c4
richardyang-gdc <richard.yang@perficient.com>
2020-07-16 10:28:13 +0800

    correct typo

--- test/stringCalculator.spec.js
+++ test/stringCalculator.spec.js
@@ -2,7 +2,7 @@ import { equal } from "assert";
 import StringCalculator from "../src/stringCalculator";
 
 describe('stringCalcutlator usage suite', () => {
-    it('should return 0 when paramter string is empty', () => {
+    it('should return 0 when parameter string is empty', () => {
         equal(StringCalculator.add(''), 0);
     });   
 
